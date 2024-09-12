bc0791614ed50a7e30e7b6db418563b74546e2e9
Sefa-Gurer <sgurer3@gmail.com>
2024-09-12 15:00:53 +0300

    saatin yazacağı ekran artık oluşan pencereye göre dinamik olarak ayarlanıyor.

--- gui.py
+++ gui.py
@@ -3,10 +3,60 @@ class Screen:
                  canvas,
                  square_corner_one,
                  square_corner_two,
-                 square_color = "blue"):
+                 square_color="blue"):
+
+        self.canvas = canvas  # Canvas'ı sakla, ileride başka şeyler çizmek için kullanılabilir
+        self.square_corner_one = square_corner_one
+        self.square_corner_two = square_corner_two
+        self.square_color = square_color
+
+
+        # for i in range(4):
+        #     "digit"+str(i) = Digits(i,)
+
 
         # Geometrik şekiller çizelim
-        canvas.create_rectangle(square_corner_one[0], square_corner_one[1], 
-                                square_corner_two[0], square_corner_two[1], 
-                                fill=square_color, outline=square_color)
+        self.draw_square()
 
+    # def find_digits_corner(self,digits_id):
+    #     """ 
+    #     digits ==>  1 2 : 1 2
+    #     id     ==>  2 1 :-1-2
+    #     """
+    #     if digits_id == 2:
+    #         corner_one = self.square_corner_one
+    #         corner_two =   
+    #     self.square_corner_one
+    #     self.square_corner_two
+
+    def draw_square(self):
+        """Bir dikdörtgen (veya kare) çizer"""
+        self.canvas.create_rectangle(
+            self.square_corner_one[0], self.square_corner_one[1], 
+            self.square_corner_two[0], self.square_corner_two[1], 
+            fill=self.square_color, outline=self.square_color,
+            tags="cerceve"
+        )
+    
+    def draw_delete(self):
+        self.canvas.delete("cerceve")
+
+class Digits:
+    def __init__(self,
+                 id,
+                 corner_one,
+                 corner_two,
+                 colors="blue"):
+
+        self.id = id
+        self.corner_one = corner_one
+        self.corner_two = corner_two
+        self.colors = colors
+        
+class Cells:
+    def __init__(self,
+                id,
+                value):
+
+        self.id = id
+        self.value = value
--- main.py
+++ main.py
@@ -1,36 +1,59 @@
 from tkinter import *
 from gui import Screen
+import time
 
 class Play:
     def __init__(self):
 
-        self.screen_width = 400
-        self.screen_height = 250
+        self.window_width = 600
+        self.window_height = 300
+
+        self.screen_ratio = 24/9
+        self.margin = 20
 
         # Ana pencereyi oluştur
-        root = Tk()
+        self.root = Tk()
 
-        screen_size = str(self.screen_width)+"x"+str(self.screen_height)
+        window_size = f"{self.window_width}x{self.window_height}"
+
         # Pencere boyutunu ve şeffaflığını ayarla
-        root.geometry(screen_size)
-        root.attributes('-alpha', 1)  # Pencereyi tam görünür yap
-        root.wm_attributes('-transparentcolor', '#ab23ff')  # Bu rengi şeffaf yap
+        self.root.geometry(window_size)
+        self.root.attributes('-alpha', 1)  # Pencereyi tam görünür yap
+        self.root.wm_attributes('-transparentcolor', '#ab23ff')  # Bu rengi şeffaf yap
 
         # Canvas'ı oluştur
-        canvas = Canvas(root, width=400, height=250, bg='#ab23ff', highlightthickness=0)
-        canvas.pack()
+        self.canvas = Canvas(self.root, width=self.window_width, height=self.window_height, bg='#ab23ff', highlightthickness=0)
+        self.canvas.pack()
 
-        Screen(canvas,
-               square_corner_one=(25,25),
-               square_corner_two=(self.screen_width-25,self.screen_height-25),
-               square_color="red")
+        screen_size = self.find_screen_size()
 
-        # Ana pencereyi başlat
-        root.mainloop()
+        self.screen = Screen(self.canvas,
+            square_corner_one=screen_size[0],
+            square_corner_two=screen_size[1],
+            square_color="red")
 
+        self.run()
 
 #---------------kalınan yer-------------------
-    # def run():
+    def run(self):
+
+        # 1000 milisaniye (1 saniye) sonra tekrar run()'u çalıştır
+        self.root.after(1000, self.run)
+
+    def find_screen_size(self):
+        if (self.window_height-(2*self.margin)) * self.screen_ratio <= (self.window_width-(2*self.margin)):
+            screen_height = self.window_height-(2*self.margin)
+            screen_width = screen_height * self.screen_ratio
+        else:
+            screen_width = self.window_width-(2*self.margin)
+            screen_height = screen_width / self.screen_ratio
+
+        corner_one = (self.window_width-screen_width)/2,(self.window_height-screen_height)/2
+        corner_two = (self.window_width-corner_one[0]),(self.window_height-corner_one[1])
+
+        return (corner_one,corner_two)
 
 if __name__ == '__main__':
     play = Play()
+    # Ana pencereyi başlat
+    play.root.mainloop()
